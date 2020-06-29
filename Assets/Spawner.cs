43289d7212b3cab07092379c180242eae5199ec9
MarcoElz <marco.eelizondo@gmail.com>
2020-06-29 14:26:38 -0500

    Meteorite Spawner

---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public float timeBetweenSpawns = 1f;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", 1f, timeBetweenSpawns);
+    }
+
+    void Spawn()
+    {
+        float x = Random.Range(-3f, 3f);
+        Vector3 position = new Vector3(x, transform.position.y, 0f);
+
+        int random = Random.Range(0, prefabs.Length);
+
+        Instantiate(prefabs[random], position, Quaternion.identity);
+    }
+
+
+}
